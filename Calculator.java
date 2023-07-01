0de2c5c75387ad2023f11dd23133f5a85ef7fc2d
athanikarsadik <128250755+athanikarsadik@users.noreply.github.com>
2023-07-01 15:24:57 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 
-
 public class Calculator extends JFrame implements ActionListener {
     double number, answer;
     int calculation;
